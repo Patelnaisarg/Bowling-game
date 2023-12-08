e1be7a9419e742fda7af82828c9aff0b1120f79f
Patelnaisarg <110363151+Patelnaisarg@users.noreply.github.com>
2023-12-08 17:06:33 +1300

    Update BowlingGame.py
    after adding proper logic and what i had changed in this selected software.

--- BowlingGame.py
+++ BowlingGame.py
@@ -1,24 +1,41 @@
 class BowlingGame:
     def __init__(self):
         self.rolls = []
+"""Initialize a new BowlingGame instance.
 
+        This method initializes the rolls list to keep track of pins knocked down.
+        """
     def roll(self, pins):
         self.rolls.append(pins)
+         """Record the number of pins knocked down in a roll.
 
-    def score(self):
+        Args:
+            pins (int): The number of pins knocked down in the roll.
+
+        Raises:
+            ValueError: If the pins value is outside the range of 0 to 10.
+        """
+
+    def score(self): #whole scoring system with total score,frame index"
         total_score = 0
         roll_index = 0
         frame_scores = []
+ """Calculate the total score of the game.
 
+        Returns:
+            list: A list containing the scores for each frame in the game.
+        """
         for frame in range(12):
             if self.is_strike(roll_index) and len(self.rolls) > roll_index + 2:
                 total_score += 10 + self.strike_bonus(roll_index)
                 frame_scores.append(total_score)
                 roll_index += 1
+                """frame index for strikes"""
             elif self.is_spare(roll_index) and len(self.rolls) > roll_index + 2:
                 total_score += 10 + self.spare_bonus(roll_index)
                 frame_scores.append(total_score)
                 roll_index += 2
+                """frame index for spares"""
             elif len(self.rolls) > roll_index + 1:
                 total_score += self.frame_score(roll_index)
                 frame_scores.append(total_score)
@@ -43,12 +60,12 @@ class BowlingGame:
     def frame_score(self, roll_index):
         return self.rolls[roll_index] + self.rolls[roll_index + 1]
 
-
+# The main function and input handling logic can be kept without specific PythonDoc comments as it's user interaction logic.
 def main():
     game = BowlingGame()
-
+# logic
     print("Welcome to the Bowling Game!")
-
+# total  12 frames
     for frame in range(12):
         while True:
             try:
@@ -62,6 +79,7 @@ def main():
 
         if roll1 < 10:
             while True:
+                #for perfect roll sytem as 4 in first pin then only 6 pins left for the second how it's working
                 try:
                     roll2 = int(input(f"Enter number of pins knocked down in frame {frame + 1}, roll 2: "))
                     if roll2 < 0 or roll2 > (10 - roll1):
